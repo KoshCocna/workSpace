e08c292f12bbbb6897ed2257fbe6900367a650d3
KoshCocna <76080450+KoshCocna@users.noreply.github.com>
2024-04-01 13:19:57 +1000

    COMMIT-09

---
+++ the-web-developer-bootcamp/clone-sites/spaceX/js/script.js
@@ -0,0 +1,52 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
+
+btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+  document.body.classList.toggle("stop-scrolling");
+  menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  console.log(scrollPos);
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = "0";
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+      const increment = target / 100;
+      if (c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = "0"));
+}
